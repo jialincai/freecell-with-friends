57fabc40d44bb6edb1946fc8e0baca5e5b815afb
Jialin Cai <jcai@mac.lan>
2025-03-27 14:57:53 -0700

    lint

--- src/phasor/card/domain/CardComparison.ts
+++ src/phasor/card/domain/CardComparison.ts
@@ -4,27 +4,34 @@ import { SUIT_COLOR } from "@phasor/constants/deck";
 export const isValidStack = (
   cards: CardData[],
   predicates: ((a: CardData, b: CardData) => boolean)[],
-): boolean =>
-  cards.every(
+): boolean => {
+  return cards.every(
     (card, index) =>
       index === 0 ||
-      predicates.every((predicates) => predicates(card, cards[index - 1])),
+      predicates.every((predicate) => predicate(card, cards[index - 1])),
   );
+}
 
-export const isSameColor = (a: CardData, b: CardData): boolean =>
-  SUIT_COLOR[a.suit] === SUIT_COLOR[b.suit];
+export const isSameColor = (a: CardData, b: CardData): boolean => {
+  return SUIT_COLOR[a.suit] === SUIT_COLOR[b.suit];
+}
 
-export const isDifferentColor = (a: CardData, b: CardData): boolean =>
-  SUIT_COLOR[a.suit] !== SUIT_COLOR[b.suit];
+export const isDifferentColor = (a: CardData, b: CardData): boolean => {
+  return SUIT_COLOR[a.suit] !== SUIT_COLOR[b.suit];
+}
 
-export const isSameSuit = (a: CardData, b: CardData): boolean =>
-  a.suit === b.suit;
+export const isSameSuit = (a: CardData, b: CardData): boolean => {
+  return a.suit === b.suit;
+}
 
-export const hasDifferentSuit = (a: CardData, b: CardData): boolean =>
-  a.suit !== b.suit;
+export const isDifferentSuit = (a: CardData, b: CardData): boolean => {
+  return a.suit !== b.suit;
+}
 
-export const isDescending = (a: CardData, b: CardData): boolean =>
-  a.value === b.value - 1;
+export const isDescending = (a: CardData, b: CardData): boolean => {
+  return a.value === b.value - 1;
+}
 
-export const isAscending = (a: CardData, b: CardData): boolean =>
-  a.value === b.value + 1;
+export const isAscending = (a: CardData, b: CardData): boolean => {
+  return a.value === b.value + 1;
+}

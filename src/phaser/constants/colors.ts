d2b4737d9f2eb3c59ea4f86c58c562f1f4c6092b
Jialin Cai <jcai@Jialins-MacBook-Pro.local>
2025-04-10 16:44:21 -0700

    update colors

--- src/phaser/constants/colors.ts
+++ src/phaser/constants/colors.ts
@@ -1,6 +1,6 @@
-export const BACKGROUND_COLOR = 0x6aaa64;
-export const BORDER_COLOR = 0xffffff;
-export const TEXT_COLOR = 0xffffff;
-export const BUTTON_TEXT_COLOR = 0x4d5766;
-export const BUTTON_COLOR = 0xffffff;
-export const HIGHLIGHT_COLOR = 0x4a90e2
+export const BACKGROUND_COLOR = 0x202a33;
+export const BORDER_COLOR = 0xf2f1ef;
+export const TEXT_COLOR = 0xf2f1ef;
+export const BUTTON_TEXT_COLOR = 0x202a33;
+export const BUTTON_COLOR = 0xf2f1ef;
+export const HIGHLIGHT_COLOR = 0x4a90e2;

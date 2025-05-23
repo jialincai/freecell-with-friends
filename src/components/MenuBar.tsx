ad635d130a223d24c31af745554baa7fad1c0151
Jialin Cai <jiali@DESKTOP-5H8CFNL>
2025-05-23 14:08:49 -0400

    add trophy icon

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,10 +1,13 @@
-import { HelpCircle } from "lucide-react";
+import { HelpCircle, Trophy} from "lucide-react";
 import styles from "@styles/MenuBar.module.css";
 
 const MenuBar: React.FC = () => (
     <div className={styles.menuBar}>
-      <button className={styles.helpButton} title="How to play">
-        <HelpCircle className={styles.helpIcon} />
+      <button className={styles.menuButton}>
+        <Trophy className={styles.menuIcon} />
+      </button>
+      <button className={styles.menuButton}>
+        <HelpCircle className={styles.menuIcon} />
       </button>
     </div>
   );

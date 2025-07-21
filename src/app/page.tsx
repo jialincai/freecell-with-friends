d1aa55c6aded20314331a7ad9f3cc2f5f7b85a4f
Jialin Cai <jiali@DESKTOP-5H8CFNL>
2025-07-21 12:54:49 -0400

    simplify session provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
 import FreecellGame from "@components/game/FreecellGame";
 import Overlay from "@components/ui/Overlay";
 import MenuBar from "@components/ui/MenuBar";
 import { Suspense } from "react";
 
 const HomePage = () => {
+  // TODO: Fetch daily deal here
+
   return (
-    <div>
-      <Suspense fallback={null}>
-        <Overlay />
-      </Suspense>
-      <MenuBar />
-      <FreecellGame />
-    </div>
+    <SessionProvider>
+      <div>
+        <Suspense fallback={null}>
+          <Overlay />
+        </Suspense>
+        <MenuBar />
+        <FreecellGame />
+      </div>
+    </SessionProvider>
   );
 };
 

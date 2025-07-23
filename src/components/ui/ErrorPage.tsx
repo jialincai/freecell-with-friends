7bd7d104f957c05a4009ec14338f20a603472d35
Jialin Cai <jiali@DESKTOP-5H8CFNL>
2025-07-23 17:31:05 -0400

    lint fix

--- src/components/ui/ErrorPage.tsx
+++ src/components/ui/ErrorPage.tsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
     <div className={styles.container}>
       <p className={styles.heading}>Oops! Something went wrong.</p>
       <p className={styles.message}>
-        We're having trouble connecting to the server.
+        We&apos;re having trouble connecting to the server.
       </p>
       <p className={styles.message}>
         Please contact us at{" "}

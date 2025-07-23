0bd0f0b6ae721e7827dbdfd7ddabce7537abf67e
Jialin Cai <jiali@DESKTOP-5H8CFNL>
2025-07-23 19:44:51 -0400

    add page reload

--- src/components/ui/Countdown.tsx
+++ src/components/ui/Countdown.tsx
@@ -2,24 +2,18 @@
 
 import { useState, useEffect } from "react";
 import { useDailyDeal } from "@components/context/DealContext";
-import { formatTime } from "@utils/Function";
+import { formatTime, getTimeUntilNextUTCDate } from "@utils/Function";
 
-const WARNING_THRESHOLD_MS = 30 * 60 * 1000; // 30 minutes
+const WARNING_THRESHOLD_MS = 5 * 60 * 1000; // 30 minutes
 const RELOAD_THRESHOLD_MS = 1;
-
-function getTimeUntilNextUTCDeal(): number {
-  const now = new Date();
-  const nextUTC = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1));
-  return nextUTC.getTime() - now.getTime();
-}
 
 const DealCountdown = () => {
   const deal = useDailyDeal();
-  const [remainingMs, setRemainingMs] = useState(getTimeUntilNextUTCDeal());
+  const [remainingMs, setRemainingMs] = useState(getTimeUntilNextUTCDate());
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const ms = getTimeUntilNextUTCDeal();
+      const ms = getTimeUntilNextUTCDate();
       if (ms <= RELOAD_THRESHOLD_MS) window.location.reload();
       setRemainingMs(ms);
     }, 1000);
@@ -27,14 +21,15 @@ const DealCountdown = () => {
     return () => clearInterval(interval);
   }, []);
 
-  const timeColor = remainingMs < WARNING_THRESHOLD_MS ? "text-red" : "text-gray";
+  const timeColor =
+    remainingMs < WARNING_THRESHOLD_MS ? "text-red" : "text-gray";
 
   // TODO: break out styles
   return (
     <div className="flex flex-row items-end gap-x-2 font-semibold">
-      <span className="">Freecell {deal.id}</span>
+      <span className="">Freecell #{deal.id}</span>
       <span className={`text-sm ${timeColor}`}>
-        updates in: {formatTime(remainingMs)}
+        ending in {formatTime(remainingMs)}
       </span>
     </div>
   );

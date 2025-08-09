e9fe58a7877ac821fb1f013e96dce659bad113d4
Jialin Cai <jiali@DESKTOP-5H8CFNL>
2025-08-08 22:11:45 -0400

    lint fixes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://freecellwithfriends.com';
+  const baseUrl = "https://freecellwithfriends.com";
 
   return [
     { url: `${baseUrl}/`, lastModified: new Date(), priority: 1.0 },

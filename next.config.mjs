03876e36dd43780744048e780774634b46883a6b
Jialin Cai <jiali@DESKTOP-5H8CFNL>
2025-08-10 11:37:54 -0400

    permanent redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    reactStrictMode: false,
+  output: 'standalone',
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.freecellwithfriends.com' }],
+        destination: 'https://freecellwithfriends.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
